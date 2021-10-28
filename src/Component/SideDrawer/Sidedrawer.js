0135822b0cb6b30351c1922f559ca1647f11cc23
github_username <you@example.com>
2021-10-28 18:02:15 +0530

    “comments”

--- src/Component/SideDrawer/Sidedrawer.js
+++ src/Component/SideDrawer/Sidedrawer.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 
 export const Sidedrawer = ({ direction }) => {
   return (
@@ -14,31 +12,8 @@ export const Sidedrawer = ({ direction }) => {
         height: 300,
       }}
     >
-     <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '20ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          error
-          id="outlined-error"
-          label="Error"
-          defaultValue="Hello World"
-        />
-        <TextField
-          error
-          id="outlined-error-helper-text"
-          label="Error"
-          defaultValue="Hello World"
-          helperText="Incorrect entry."
-        />
-      </div>
-      
-    </Box>
+      <Button sx={{ color: "primary.dark" }}>X</Button>
+      <p>lorepsum</p>
     </Drawer>
   );
 };
--- src/stories/Create.stories.jsx
+++ src/stories/Create.stories.jsx
@@ -2,7 +2,7 @@ import {Create1} from "../Component/Create/Create1";
 import React from 'react';
 
 export default {
-    title: 'New/Create',
+    title: 'New/Create1',
     component: Create1,
   };
-  export const create1 = ()=> <Create1/>;
+export const Create_Story = ()=> <Create1 direction="right"/>;
